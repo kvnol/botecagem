14e88ae3a7e9c5487602bfb61ecedeb7eff10690
Kevin Oliveira <me@kevinoliveira.com.br>
2017-05-22 23:14:54 -0300

    Add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+
+const files = {
+  css: ['src/assets/sass/main.sass'],
+  js: ['src/assets/js/**/*.js']
+};
+
+gulp.task('views', function() {
+  return gulp.src('src/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('styles', function() {
+  return gulp.src(files.css)
+    .pipe(sass())
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('dist/assets/css'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src(files.js)
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/assets/js'));
+});
+
+gulp.task('default', ['views', 'styles', 'scripts']);
